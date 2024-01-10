a5bcbe7fa9f550fdf7f7e3b4cca251fc6878acbf
Hossein.M <hossein.m9877@gmail.com>
2024-01-10 14:32:30 +0330

    feat: add configuration file

---
+++ config/HLW811x_config.h
@@ -0,0 +1,50 @@
+/**
+ **********************************************************************************
+ * @file   HLW811x_config.h
+ * @author Hossein.M (https://github.com/Hossein-M98)
+ * @brief  Project specific configurations
+ **********************************************************************************
+ *
+ * Copyright (c) 2024 Mahda Embedded System (MIT License)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ **********************************************************************************
+ */
+  
+/* Define to prevent recursive inclusion ----------------------------------------*/
+#ifndef _HLW811X_CONFIG_H_
+#define _HLW811X_CONFIG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Configurations ---------------------------------------------------------------*/
+#define HLW811X_CONFIG_SUPPORT_SPI    1
+#define HLW811X_CONFIG_SUPPORT_UART   1
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //! _HLW811X_CONFIG_H_
